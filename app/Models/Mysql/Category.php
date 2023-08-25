7350f3a5f842effc3ab8e0d2392547dd56b3d9d3
Yan Gonçalves <yanw100@gmail.com>
2023-08-25 07:40:15 -0300

    feature: Adiciona relacionamento

--- app/Models/Mysql/Category.php
+++ app/Models/Mysql/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Mysql;
 
+use App\Models\Mongo\Transaction;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -14,5 +15,10 @@ class Category extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function transactions(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
